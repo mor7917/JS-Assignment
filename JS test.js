7bd935f68646e51d81788614f2fa672c6a220bbf
mor7917 <139258838+mor7917@users.noreply.github.com>
2024-05-22 18:24:57 +0530

    Create JS test.js

---
+++ JS test.js
@@ -0,0 +1,34 @@
+
+const NFTs = []
+
+function mintNFT (_name,_eyeColor,_shirtType,_bling) {
+    const NFT ={
+        "name": _name,
+        "eyeColor": _eyeColor,
+        "shirtType": _shirtType,
+        "bling": _bling,
+    }
+    NFTs.push(NFT);
+    console.log("Minted: "+ _name);
+}
+
+function listNFTs () {
+for(let i=0; i<NFTs.length;i++){
+    console.log("\n ID:\t\t\t\t"+(i+1));
+    console.log(" Name:\t\t\t" + NFTs[i].name);
+    console.log(" Eyecolor:\t" +NFTs[i].eyeColor);
+    console.log(" Shirt Type:" +NFTs[i].shirtType);
+    console.log(" Bling:\t\t\t" +NFTs[i].bling);
+}
+}
+
+function getTotalSupply() {
+console.log("\n" + NFTs.length);
+}
+
+mintNFT("Bob","Blue","Hoodie","Gold Chain");
+mintNFT("Tim","Black","Shirt","Gold Chain");
+mintNFT("Jim","Green","Jacket","Gold Chain");
+mintNFT("Kim","White","Coat","Gold Chain");
+listNFTs();
+getTotalSupply();
